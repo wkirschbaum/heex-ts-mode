78624cd2aa248c4b44be9167dcc7d819747bc05b
Wilhelm H Kirschbaum <wkirschbaum@gmail.com>
2023-03-16 22:43:08 +0200

    Fix source file headers for some tests

--- test/heex-ts-mode-tests.el
+++ test/heex-ts-mode-tests.el
@@ -1,3 +1,22 @@
+;;; heex-ts-mode-tests.el --- Tests for elixir-ts-mode         -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2022, 2023 Wilhelm H Kirschbaum
+
+;;  This program is free software: you can redistribute it and/or modify
+;;  it under the terms of the GNU General Public License as published by
+;;  the Free Software Foundation, either version 3 of the License, or
+;;  (at your option) any later version.
+
+;;  This program is distributed in the hope that it will be useful,
+;;  but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;  GNU General Public License for more details.
+
+;;  You should have received a copy of the GNU General Public License
+;;  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Code:
+
 (require 'ert)
 (require 'ert-x)
 (require 'treesit)
@@ -7,3 +26,4 @@
   (ert-test-erts-file (ert-resource-file "indent.erts")))
 
 (provide 'heex-ts-mode-tests)
+;;; heex-ts-mode-tests.el ends here
